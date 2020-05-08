183e12e17384bee80484fda04ebf917b635782b7
Hajesha <hkandasamy@ryerson.ca>
2020-05-08 13:46:08 -0400

    use colorthief module instead since time payoff isn't that much different but it gets a more accurate representation

--- processScreen.py
+++ processScreen.py
@@ -1,37 +1,12 @@
-import numpy as np
-import cv2
 import time
-from sklearn.cluster import MiniBatchKMeans
+from colorthief import ColorThief
 
 
-def get_colors(hist, centroids):
-    show_color = None
-
-    for (percent, color) in zip(hist, centroids):
-        show_color = color.astype("uint8").tolist()
-
-    return show_color
+def get_colors_using_colorthief():
+    color_thief = ColorThief('test.png')
+    return color_thief.get_color(quality=1)
 
 
-def centroid_histogram(clt):
-    num_labels = np.arange(0, len(np.unique(clt.labels_)) + 1)
-    (hist, _) = np.histogram(clt.labels_, bins=num_labels)
-
-    hist = hist.astype("float")
-    hist /= hist.sum()
-
-    return hist
-
-
-frame = cv2.imread("test.jpg")
 start = time.perf_counter()
-image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-image = image.reshape((image.shape[0] * image.shape[1], 3))
-
-clt = MiniBatchKMeans(n_clusters=1, max_iter=10, n_init=1)
-clt.fit(image)
-hist = centroid_histogram(clt)
-color = get_colors(hist, clt.cluster_centers_)
+print(get_colors_using_colorthief())
 end = time.perf_counter()
-print(color)
-print(f"Took {end - start:0.4f} seconds")
